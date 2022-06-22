81303547afa6070537e3242a914a4fe0aea9bba2
kim jong seong <dkdleldl1@gmail.com>
2022-06-22 15:34:25 +0900

    uuid 테스트

---
+++ part03/src/koreait/exT/UUIDTest.java
@@ -0,0 +1,23 @@
+package koreait.exT;
+
+import java.util.UUID;
+
+public class UUIDTest {
+
+	public static void main(String[] args) {
+		
+		
+		String uuid = UUID.randomUUID().toString();
+		//16진수 32자리 + 하이픈 기호 4개로 만들어지는 랜덤문자열
+		for(int i=0;i<10;i++)
+			System.out.println(uuid);
+		
+		for(int i=0;i<10;i++)
+			System.out.println(uuid.replace("-", ""));
+		
+		for(int i=0;i<10;i++)
+			uuid = UUID.randomUUID().toString();
+		System.out.println(uuid.replace("-", "").substring(0, 12));
+	}
+
+}
